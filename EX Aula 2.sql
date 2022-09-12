afa2130fb34edfc2f2ded4ab4e6920be24b091a6
luccarojasc <59325078+luccarojasc@users.noreply.github.com>
2022-09-11 21:31:27 -0300

    Add files via upload

---
+++ EX Aula 2.sql
@@ -0,0 +1,21 @@
+create table Fornecedor(
+	Fcodigo int NOT NULL,
+	Fnome varchar(15) NOT NULL,
+	Fcidade varchar(20) not null,
+	constraint pkFornecedor primary key (Fcodigo)
+);
+create table Peca(
+	Pcodigo int NOT NULL,
+	Pnome varchar(15) NOT NULL,
+	Pdescricao varchar(50) NOT NULL,
+	constraint pkPeca primary key (PCodigo)
+);
+create table Venda(
+	codForn int NOT NULL,
+	codPeca int NOT NULL,
+	Vquantidade int,
+	Vdata date,
+	constraint fkFcodigo foreign key (codForn) references Fornecedor(Fcodigo),
+	constraint fkPcodigo foreign key (codPeca) references Peca(Pcodigo)
+);
+
